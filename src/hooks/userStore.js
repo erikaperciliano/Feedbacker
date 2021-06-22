0b987d9feb2dcc5e4586b2f43968e2cf285153a2
Erika <erikaperciliano.developer@gmail.com>
2021-06-21 23:20:37 -0300

    add hooks

--- src/hooks/userStore.js
+++ src/hooks/userStore.js
@@ -1,7 +1,7 @@
 import Store from '../store'
 
-export default function useStore (module){
-  if(module){
+export default function useStore (module) {
+  if (module) {
     return Store[module]
   }
 
