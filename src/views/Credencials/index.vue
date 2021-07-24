e648bb43f6dbe0dc2a2e5fa0cf7bb584fb433c57
Erika <erikaperciliano.developer@gmail.com>
2021-07-24 19:39:06 -0300

    finishing up the screen of Credentials

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -28,15 +28,18 @@
       <div
         v-else
         class="flex py-3 pl-5 pr-20 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
-        <span>{{ store.User.currentUser.apiKey }}</span>
-        <div class="flex ml-20 mr-5">
+        <span v-if="state.hasError">Error ao recarregar a apiKey</span>
+        <span v-else>{{ store.User.currentUser.apiKey }}</span>
+        <div class="flex ml-20 mr-5" v-if="!state.hasError">
           <icon
+            @click="handleCopy"
             name="copy"
             color="{{brandColors.graydark}}"
             size="24"
             class="cursor-pointer"
           />
           <icon
+            @click="handleGenerateApiKey"
             name="loading"
             color="{{brandColors.graydark}}"
             size="24"
@@ -57,32 +60,76 @@
       <div
         v-else
         class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll">
-        <pre>&lt;script src="https://erikaperciliano-feedbacker-widget.netlify.app?apiKey={{store.User.currentUser.apiKey}}&gt;&lt;/script&gt;"</pre>
+        <span v-if="state.hasError">Error ao recarregar o script</span>
+        <pre v-else>&lt;script src="https://erikaperciliano-feedbacker-widget.netlify.app?apiKey={{store.User.currentUser.apiKey}}&gt;&lt;/script&gt;"</pre>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { reactive, watch } from 'vue'
+import { useToast } from 'vue-toastification'
 import HeaderLogged from '../../components/HeaderLogged'
 import Icon from '../../components/Icon'
 import useStore from '../../hooks/useStore'
 import palette from '../../../palette'
 import ContentLoader from '../../components/ContentLoader'
-import { reactive } from 'vue'
+import services from '../../services'
+import { setApiKey } from '../../store/user'
 
 export default {
   components: { HeaderLogged, Icon, ContentLoader },
 
   setup () {
     const store = useStore()
+    const toast = useToast()
     const state = reactive({
+      hasError: false,
       isLoading: false
     })
+
+    watch(() => store.User.currentUser, () => {
+      if (!store.Global.isLoading && !store.User.currentUser.apiKey) {
+        handleError(true)
+      }
+    })
+
+    function handleError (error) {
+      state.isLoading = false
+      state.hasError = !!error
+    }
+
+    async function handleGenerateApiKey () {
+      try {
+        state.isLoading = true
+
+        const { data } = await services.users.generateApiKey()
+
+        setApiKey(data.apiKey)
+
+        state.isLoading = false
+      } catch (error) {
+        handleError(error)
+      }
+    }
+
+    async function handleCopy () {
+      toast.clear()
+
+      try {
+        await navigator.clipboard.writeText(store.User.currentUser.apiKey)
+        toast.success('Copiado!')
+      } catch (error) {
+        handleError(error)
+      }
+    }
 
     return {
       state,
       store,
-      brandColors: palette.brand
+      brandColors: palette.brand,
+      handleGenerateApiKey,
+      handleCopy
     }
   }
 }
