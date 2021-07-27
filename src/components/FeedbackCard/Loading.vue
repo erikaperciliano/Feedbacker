76456c1c729956ae81317782051eca821c045db8
Erika <erikaperciliano.developer@gmail.com>
2021-07-26 21:45:36 -0300

    add new component

---
+++ src/components/FeedbackCard/Loading.vue
@@ -0,0 +1,35 @@
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded"
+    width="100%"
+    heigth="300px"
+  >
+  <content-loader
+    class="mt-3 rounded"
+    width="90%"
+    heigth="90px"
+    animation-duration="2s"
+  />
+   <content-loader
+    class="mt-3 rounded"
+    width="90%"
+    heigth="90px"
+    animation-duration="2.2s"
+  />
+   <content-loader
+    class="mt-3 rounded"
+    width="90%"
+    heigth="90px"
+    animation-duration="2.7s"
+  />
+  </content-loader>
+</template>
+
+<script>
+import ContentLoader from '../ContentLoader'
+export default {
+  components: {
+    ContentLoader
+  }
+}
+</script>
