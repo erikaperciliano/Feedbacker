c013284eda8637b59b337c8b08bc159f86d3f1c1
Erika <erikaperciliano.developer@gmail.com>
2021-07-27 21:24:41 -0300

    get value of feedback

--- src/components/FeedbackCard/index.vue
+++ src/components/FeedbackCard/index.vue
@@ -7,30 +7,79 @@
       <badge :type="feedback.type"/>
 
       <span class="font-regular text-brand-graydark">
-        {{ feedback.createdAt }}
+        {{ getDiffTimeBetweenCurrent(feedback.createdAt) }}
       </span>
+    </div>
+
+    <div class="text-lg font-medium text-gray-800">
+      {{feedback.text}}
+    </div>
+
+    <div
+    :class="{ animate__fadeOutUp: state.isClosing }"
+      class="flex mt-8 animate__animated__fodeInUp animate__faster"
+      v-if="state.isOpen"
+    >
+      <div class="felx flex-col w-1/2">
+        <div class="flex felx-col">
+          <span class="font-bold text-gray-400 uppercase select-none">Página</span>
+          <span class="font-medium text-gray-700">{{feedback.page }}</span>
+        </div>
+        <div class="flex felx-col">
+          <span class="font-bold text-gray-400 uppercase select-none">Dispositivo</span>
+          <span class="font-medium text-gray-700">{{feedback.device }}</span>
+        </div>
+
+        <div class="flex felx-col">
+          <span class="font-bold text-gray-400 uppercase select-none">Usuário</span>
+          <span class="font-medium text-gray-700">{{feedback.fingerprint }}</span>
+        </div>
+      </div>
+
+      <div class="flex justify-end mt-8" v-if="!state.isOpen">
+        <icon name="chevron-down" size="24" :color="brandColors.graydark"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import Icon from '../Icon'
 import Badge from './Badge'
+import getDiffTimeBetweenCurrent from '../../utils/getDiffTimeBetweenCurrentDate'
+import palette from '../../../palette'
+import { wait } from '../../utils/timeout'
 
 export default {
   components: {
-    Badge
+    Badge,
+    Icon
   },
   props: {
+    isOpened: { type: Boolean, default: false },
     feedback: { type: Object, required: true }
   },
 
-  setup () {
-    function handleToggle () {
+  setup (props) {
+    const state = reactive({
+      isOpen: props.isOpened,
+      isClosing: !props.isOpened
+    })
 
+    async function handleToggle () {
+      state.isClosing = true
+
+      await wait(250)
+      state.isOpen = !state.isOpen
+      state.isOpen = !state.isOpen
     }
 
     return {
-      handleToggle
+      state,
+      handleToggle,
+      getDiffTimeBetweenCurrent,
+      brandColors: palette.brand
     }
   }
 }
