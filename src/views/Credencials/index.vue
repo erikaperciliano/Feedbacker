cbe4dc4a9cadd5ea98d02069b7a10b454b341dff
Erika <erikaperciliano.developer@gmail.com>
2021-08-01 19:57:05 -0300

    test end2end with cypress on Credencials

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -29,7 +29,7 @@
         v-else
         class="flex py-3 pl-5 pr-20 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
         <span v-if="state.hasError">Error ao recarregar a apiKey</span>
-        <span v-else>{{ store.User.currentUser.apiKey }}</span>
+        <span v-else id="apikey">{{ store.User.currentUser.apiKey }}</span>
         <div class="flex ml-20 mr-5" v-if="!state.hasError">
           <icon
             @click="handleCopy"
@@ -39,6 +39,7 @@
             class="cursor-pointer"
           />
           <icon
+            id="generate-apikey"
             @click="handleGenerateApiKey"
             name="loading"
             color="{{brandColors.graydark}}"
